744fb7f54b58f1f2d37c5a600734bdb9344228ff
Gildeir <gildeirlopes@gmail.com>
2021-11-04 16:05:47 -0300

    Iniciando projeto

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import TodoForm from './components/TodoForm';
+import { useState } from 'react'
+
+
+function App() {
+  return (
+    <div className="App">
+      <TodoForm />
+    </div>
+  );
+}
+
+export default App;
